12cc7d3b668794d202847ac09eaa27d59fe093a4
Houmadi Hairou <hairou.houmadi@gmail.com>
2025-06-11 10:38:32 +0200

    save message form sucess

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,21 +1,25 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+  const [success, setSuccess] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
       setSending(true);
-      // We try to call mockContactApi
+      setSuccess(false);
       try {
         await mockContactApi();
         setSending(false);
+        setSuccess(true);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -23,44 +27,62 @@ const Form = ({ onSuccess, onError }) => {
     },
     [onSuccess, onError]
   );
+    useEffect(() => {
+    let timer;
+    if (success) {
+      timer = setTimeout(() => {
+        setSuccess(false);
+      }, 3000); // 3 secondes d'affichage
+    }
+    return () => clearTimeout(timer);
+  }, [success]);
+
   return (
-    <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
-            selection={["Personel", "Entreprise"]}
-            onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
-            titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
-          </Button>
+    <>
+      <form onSubmit={sendContact}>
+        <div className="row">
+          <div className="col">
+            <Field placeholder="" label="Nom" />
+            <Field placeholder="" label="Prénom" />
+            <Select
+              selection={["Personel", "Entreprise"]}
+              onChange={() => null}
+              label="Personel / Entreprise"
+              type="large"
+              titleEmpty
+            />
+            <Field placeholder="" label="Email" />
+            <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+              {sending ? "En cours" : "Envoyer"}
+            </Button>
+          </div>
+          <div className="col">
+            <Field
+              placeholder="message"
+              label="Message"
+              type={FIELD_TYPES.TEXTAREA}
+            />
+          </div>
         </div>
-        <div className="col">
-          <Field
-            placeholder="message"
-            label="Message"
-            type={FIELD_TYPES.TEXTAREA}
-          />
+      </form>
+
+      {success && (
+        <div className="Form__successMessage">
+          ✅ Votre message a bien été envoyé.
         </div>
-      </div>
-    </form>
+      )}
+    </>
   );
 };
 
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
